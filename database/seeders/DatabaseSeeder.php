c1249ce96bb7d074bbed811d2acbfa72b8acc792
shipetopic <antonkr357@gmail.com>
2022-05-19 13:20:41 -0400

    lec123: Model Relations Inside Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\BlogPost;
+use App\Models\Comment;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
@@ -15,24 +18,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(1)->create(
-            [
-                'name' => 'John Doe',
-                'email' => 'john@laravel.tesz',
-                'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'remember_token' => Str::random(10),
-            ]
-        );
+        // # Add 1 specific user
+        // \App\Models\User::factory(1)->create(
+        //     [
+        //         'name' => 'John Doe',
+        //         'email' => 'john@laravel.tesz',
+        //         'email_verified_at' => now(),
+        //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //         'remember_token' => Str::random(10),
+        //     ]
+        // );
 
-        // $user_factory_obj = new UserFactory();
-        
-        // DB::table('users')->insert([
-        //     'name' => $user_factory_obj->faker->name,
-        //     'email' => $user_factory_obj->faker->unique()->safeEmail,
-        //     'email_verified_at' => now(),
-        //     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        //     'remember_token' => Str::random(10),
-        // ]);
+        # Add 1 specific user - but using 'state'
+        $doe = \App\Models\User::factory()->newJohnDoeUser()->create();
+
+        # Add 10 random users
+        $else = \App\Models\User::factory(20)->create();
+
+        $users = $else->concat([$doe]);
+
+        $posts = BlogPost::factory(50)->make()->each(function($post) use ($users){
+            $post->user_id = $users->random()->id;
+            $post->save();
+        });
+
+        $comments = Comment::factory(150)->make()->each(function($comment) use ($posts){
+            $comment->blog_post_id = $posts->random()->id;
+            $comment->save();
+        });
     }
 }
