f3ee0d6408eb6cc1d89d0bec588f26f8a62c210c
shipetopic <antonkr357@gmail.com>
2022-05-19 13:34:22 -0400

    lec124: Individual Seeder Classes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,33 +18,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // # Add 1 specific user
-        // \App\Models\User::factory(1)->create(
-        //     [
-        //         'name' => 'John Doe',
-        //         'email' => 'john@laravel.tesz',
-        //         'email_verified_at' => now(),
-        //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        //         'remember_token' => Str::random(10),
-        //     ]
-        // );
-
-        # Add 1 specific user - but using 'state'
-        $doe = \App\Models\User::factory()->newJohnDoeUser()->create();
-
-        # Add 10 random users
-        $else = \App\Models\User::factory(20)->create();
-
-        $users = $else->concat([$doe]);
-
-        $posts = BlogPost::factory(50)->make()->each(function($post) use ($users){
-            $post->user_id = $users->random()->id;
-            $post->save();
-        });
-
-        $comments = Comment::factory(150)->make()->each(function($comment) use ($posts){
-            $comment->blog_post_id = $posts->random()->id;
-            $comment->save();
-        });
+        # Calls specific seeders in order (specified in array)
+        $this->call([
+            UsersTableSeeder::class,
+            BlogPostsTableSeeder::class,
+            CommentsTableSeeder::class,
+        ]);
     }
 }
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // # Add 1 specific user
+        // \App\Models\User::factory(1)->create(
+        //     [
+        //         'name' => 'John Doe',
+        //         'email' => 'john@laravel.tesz',
+        //         'email_verified_at' => now(),
+        //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //         'remember_token' => Str::random(10),
+        //     ]
+        // );
+
+        # Add 1 specific user - but using 'state' named in this case: "newJohnDoeUser"
+        \App\Models\User::factory()->newJohnDoeUser()->create();
+
+        # Add 10 random users
+        \App\Models\User::factory(20)->create();
+    }
+}
