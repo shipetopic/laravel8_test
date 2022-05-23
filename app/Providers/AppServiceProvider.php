ca98ff9543f9333135a3445b090df792a1813a5e
shipetopic <antonkr357@gmail.com>
2022-05-23 12:05:26 -0400

    lec154: Complicated example of conditional rendering

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,5 +28,6 @@ class AppServiceProvider extends ServiceProvider
         // Blade::aliasComponent('badge', Badge::class);
         Blade::aliasComponent('components.badge', 'badge');
         Blade::aliasComponent('components.updated', 'updated');
+        Blade::aliasComponent('components.card', 'card');
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -15,14 +15,14 @@
         <div class="col-4">
             <div class="container">
                 <div class="row">
-                    <div class="card" style="width: 100%;">
-                        <div class="card-body">
-                            <h5 class="card-title">Most Commented</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">
-                                What people are currently talking about
-                            </h6>
-                        </div>
-                        <ul class="list-group list-group-flush">
+                    <x-card>
+                        @slot('title')
+                            Most Commented
+                        @endslot
+                        @slot('subtitle')
+                            What people are currently talking about
+                        @endslot
+                        @slot('items')
                             @foreach ($mostCommented as $post)
                                 <li class="list-group-item">
                                     <a href="{{ route('posts.show', ['post' => $post->id]) }}">
@@ -30,44 +30,32 @@
                                     </a>
                                 </li>
                             @endforeach
-                        </ul>
-                    </div>
+                        @endslot
+                    </x-card>
                 </div>
 
                 <div class="row mt-4">
-                    <div class="card" style="width: 100%;">
-                        <div class="card-body">
-                            <h5 class="card-title">Most Active</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">
-                                Users with most posts written
-                            </h6>
-                        </div>
-                        <ul class="list-group list-group-flush">
-                            @foreach ($mostActive as $user)
-                                <li class="list-group-item">                                    
-                                    {{ $user->name }}                                    
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div>
+                    <x-card>
+                        @slot('title')
+                            Most Active
+                        @endslot
+                        @slot('subtitle')
+                            Users with most posts written
+                        @endslot
+                        @slot('items', collect($mostActive)->pluck('name'))
+                    </x-card>
                 </div>
 
                 <div class="row mt-4">
-                    <div class="card" style="width: 100%;">
-                        <div class="card-body">
-                            <h5 class="card-title">Most Active Last Month</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">
-                                Users with most posts written in the Last Month
-                            </h6>
-                        </div>
-                        <ul class="list-group list-group-flush">
-                            @foreach ($mostActiveLastMonth as $user)
-                                <li class="list-group-item">                                    
-                                    {{ $user->name }}                                    
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div>
+                    <x-card>
+                        @slot('title')
+                            Most Active Last Month
+                        @endslot
+                        @slot('subtitle')
+                            Users with most posts written in the Last Month
+                        @endslot
+                        @slot('items', collect($mostActiveLastMonth)->pluck('name'))
+                    </x-card>
                 </div>
             </div>
         </div>
