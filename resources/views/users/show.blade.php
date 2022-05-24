2e3ece4d304bdf24535d2eb94969ec50e9244bd4
shipetopic <antonkr357@gmail.com>
2022-05-24 17:58:47 -0400

    lec198: Practical: OneToOne Plymorphic with BlogPost and Image

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="row">
         <div class="col-4">
-            <img src="" alt="" class="img-thumbnail avatar">
+            <img src="{{ $user->image ? $user->image->url() : '' }}" alt="" class="img-thumbnail avatar">
         </div>
 
         <div class="col-8">
