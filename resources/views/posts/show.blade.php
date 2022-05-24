d7835a6135c001bfb7b2b5acfd531ba158bb9fcf
shipetopic <antonkr357@gmail.com>
2022-05-24 15:41:16 -0400

    lec187: Uploaded image URL

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -14,6 +14,8 @@
         </h1>
         <p>{{ $post->content }}</p>
 
+        <img src="{{ $post->image->url() }}"/>
+
         {{-- <p>Added {{ $post->created_at->diffForHumans() }}</p> --}}
         <x-updated :date='$post->created_at' :name='$post->user->name'>
         </x-updated>
